9d80f7f447c2c74e91fda717b17add7e59364ec3
AaronCaoZJ <1060919265@qq.com>
2025-10-03 12:56:06 +0800

    Try TRT-LLM

---
+++ tensorrt/build_trtllm.sh
@@ -0,0 +1,4 @@
+python /home/zhijun/Code/TensorRT-LLM/examples/models/core/qwen/convert_checkpoint.py \
+    --model_dir /home/zhijun/Code/Bagel/models/BAGEL-7B-MoT \
+    --output_dir /home/zhijun/Code/Bagel/models/BAGEL-7B-MoT-trtllm \
+    --dtype float16
